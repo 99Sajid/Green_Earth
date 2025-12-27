d414bc312303fe32a2a62987395d8dd65dc3ebd4
Md. Shajid Rahman <shajiedrahman@gmail.com>
2025-12-27 16:52:12 +0600

    added at to the cart section, campign and impact section at the bottom

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const loadallCards =()=>{
 
 displayallCards =(data)=>{
     const cardcontainer = document.getElementById('card-container');
-    cardcontainer.innerHTML = '';
+    cardcontainer.innerHTML = "";
 
     data.plants.forEach(element => {
         const div = document.createElement('div');
         div.innerHTML=`
-               <div class="card bg-base-100 w-68 shadow-sm">
+               <div class="card bg-base-100 w-68 shadow-sm ">
   <figure>
     <img class="w-full h-50 p-2 rounded-xl"
       src="${element.image} "
@@ -19,13 +19,13 @@ displayallCards =(data)=>{
   </figure>
   <div class="card-body">
     <h2 class="card-title">${element.name}</h2>
-    <p>${element.description}</p>
+    <p class="text-justify max-w-3xl">${element.description}</p>
     <div class="flex justify-between">
     <h3 class="p-1 bg-red-500 rounded-2xl">${element.category}</h3>
     <h3><i class="fa-solid fa-bangladeshi-taka-sign"></i>${element.price}</h3>
     </div>
     <div class="card-actions justify-center ">
-      <button class="btn btn-primary w-full rounded-2xl bg-green-600 p-3 text-white">Add to Card</button>
+      <button class="btn btn-primary w-full rounded-2xl bg-green-600 p-3 text-white" onclick="HistoryCard('${element.name}',${element.price})">Add to Card</button>
     </div>
   </div>
 </div>
@@ -38,6 +38,65 @@ displayallCards =(data)=>{
 
 }
 
+const CatagorySelection =(id)=>{
+
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then(res=>res.json())
+    .then(data=>displayallCards(data));
+    
+
+};
+let totalamount =0;
+
+
+const HistoryCard =(name,price)=>{
+  totalamount = totalamount + price;
+  const totalAmountElement = document.getElementById('totalAmount');
+  totalAmountElement.innerText= totalamount;
+  const HistoryCard = document.getElementById('historyCard');
+  const hsdiv = document.createElement('div');
+  hsdiv.innerHTML=`
+                   <div class="card bg-[#F0FDF4] w-auto shadow-sm mb-4 hcard">
+  <div class="card-body">
+    <div class="card-actions flex flex-row justify-between items-center">
+    <div class="flex flex-col">
+    <p class="justify-start text-lg font-semibold">${name}</p>
+    <p><i class="fa-solid fa-bangladeshi-taka-sign"></i>${price}</p>
+    </div>
+      
+      <button class="btn btn-square btn-sm" id="closeBtn" onclick="removeamount(this,${price})">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-3 w-3"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </button>
+    
+    
+    </div>
+  </div>  
+</div>
+  `;
+  HistoryCard.appendChild(hsdiv);
+ 
+}
+
+ const removeamount =(btn,price)=>{
+    btn.closest('.hcard').remove() ;
+    console.log(price);
+    totalamount = totalamount - price;
+const totalAmountElement = document.getElementById('totalAmount');
+  totalAmountElement.innerText= totalamount;
+ } 
+
+
 const loadallCatagories =()=>{
     fetch("https://openapi.programming-hero.com/api/categories")
     .then(res=>res.json())
@@ -47,14 +106,13 @@ displayallCatagories =(data)=>{
 
     // console.log(data);
     const catagoriesContainer = document.getElementById('catagories-container');
-    catagoriesContainer.innerHTML = '';
+    // catagoriesContainer.innerHTML = '';
 
-    const catlength=data.categories.length;
-    // console.log(catlength);
     data.categories.forEach(element => {
-        const div = document.createElement('div');
+        const div = document.createElement('div'); 
         div.innerHTML=`
-        <button class="btn w-50">${element.category_name}</button>`
+        <button onclick="CatagorySelection('${element.id}')" class="btn w-50">${element.category_name}</button>
+        `
 
         catagoriesContainer.append(div);
     });
